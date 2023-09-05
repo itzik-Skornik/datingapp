77cb7daf42944606d53aad2d2a4f7d7c9d5f4091
= <=>
2023-09-05 10:26:04 +0300

    05/09/2023

---
+++ API/-- SQLite.sql
@@ -0,0 +1,9 @@
+-- SQLite
+INSERT INTO Users (Id, UserName)
+VALUES (1,"itzik");
+
+INSERT INTO Users (Id, UserName)
+VALUES (2,"dina");
+
+INSERT INTO Users (Id, UserName)
+VALUES (3,"thila");
---
+++ API/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace API.Controllers;
+[ApiController]
+[Route("api/[controller]")] // http/user
+public class UserController : ControllerBase
+{
+    private  DataContext _Context ;
+    public UserController(DataContext context)
+    {
+        _Context = context;
+    }
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Appuser>>> GetUsers()
+    {
+        var users = await _Context.Users.ToListAsync();
+        return users;
+    }
+    [HttpGet("{id}")] // http/users/2
+    public async Task<ActionResult<Appuser>> GetUser(int id)
+    {
+        var user = await _Context.Users.FindAsync(id);
+        return user;
+
+    }
+}
