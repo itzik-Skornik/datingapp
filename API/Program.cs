cfe8e79748cdaa38dfb1d82f400c625cb1311e4c
= <=>
2023-09-06 12:29:23 +0300

    06/09/2023

--- API/Program.cs
+++ API/Program.cs
@@ -12,7 +12,7 @@ builder.Services.AddDbContext<DataContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 }
 );
-
+builder.Services.AddCors();
 
 var app = builder.Build();
 
@@ -21,7 +21,7 @@ var app = builder.Build();
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
-
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 app.MapControllers();
 
 app.Run();
